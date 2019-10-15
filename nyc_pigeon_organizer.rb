f66b970dfa45050f5a1e66df8abe939d32ee803d
Dawn Cronin <dawncronin7@gmail.com>
2019-10-15 19:25:03 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,17 +1,21 @@
 def nyc_pigeon_organizer(data)
   
-  org_hash = {}
-    data.each do |k, v|
-    attribute = k
-    v.each do |key, value|
-      specific = key
-      value.each do |name|
-        if org_hash[name] == nil
-          org_hash[name] = {}
+    org_hash = {}
+      data.each do |k, v|
+      attribute = k
+      v.each do |key, value|
+        specific = key
+        value.each do |name|
+          if org_hash[name] == nil
+            org_hash[name] = {}
+          end
+          if org_hash[name][attribute]
+            org_hash[name][attribute] << specific.to_s
+        else
+          org_hash[name][attribute] = [specific.to_s]
         end
-        org_hash[name][attribute] = specific
+        end
       end
     end
+    return org_hash
   end
-  return org_hash
-end
